1b5138f2095a46aa3c44184c0b721ae4b5e35ca5
Bikram Jethi <bikramjethi@gmail.com>
2025-10-04 12:56:04 +0530

    add looping mechanism with a break

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,31 @@
+use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-use rand::Rng;
 
 fn main() {
     println!("Developing a guessing game!");
-    println!("Please input your guess:");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    let mut guess:String = String::new();
+    loop {
+        println!("Please input your guess:");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read user's guess");
+        let mut guess: String = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read user's guess");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("User guessed: {guess}");
 
-    println!("User guessed: {guess}");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!!"),
-        Ordering::Greater => println!("Too big!!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
