08b7d532eefdc9f74765f4a605853e1c873a0322
Bikram Jethi <bikramjethi@gmail.com>
2025-10-20 11:18:41 +0530

    cannot creating multiple mutable references

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,22 @@
-const x: i32 = 5;
+const X: i32 = 5;
 fn main() {
-    println!("Hello, world! {}", x);
+    println!("Hello, world! {}", X);
+    test_references();
+}
+
+/**
+ * This is just a test function I've created as I was going through the
+ * references section of the rust handbook
+ *
+ * I just need this to test a few hypothesis I have while I am reading the handbook
+ */
+fn test_references() {
+    println!("Test references is being called");
+
+    let mut s1 = String::from("hello");
+
+    let s2 = &mut s1;
+    let s3 = &mut s1;
+
+    println!("{s2}, {s3}");
 }
